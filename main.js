9ee7a79836a4067d33da7e3aa7211009089676ad
YujiTm <128611437+YujiTm@users.noreply.github.com>
2024-06-14 08:55:34 -0300

    14/06

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo")
 const contador = document.querySelectorAll(".contador")
+contador[0].textContent= "ola"
+const tempoObjetivo1 = new Date(2028,06,23,12,17)
+const tempoObjetivo2 = new Date(2028,08,06,)
+const tempoobjetivo3 = new Date (06)
+const tempoobletivo4= new Date ()
 
 for (let i = 0 ; i < botoes.length ; i++){
     botoes[i].onclick = function (){
-        for (let j = 0; j < botoes.length ; j++){
+        for (let j = 0; j <c botoes.length ; j++){
             botoes[j].classList.remove("ativo")
             conteudo[j].classList.remove("ativo")
         }
